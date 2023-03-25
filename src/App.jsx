de3b1d043b1d775bba5a11f9139f59a54d28942a
shubhamlahoti01 <lahotishubham03@gmail.com>
2023-03-25 23:09:21 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Example1 from './components/Example1';
+import Example2 from './components/Example2';
+
+const App = () => {
+  // return <Example1 />;
+  return <Example2 />;
+};
+
+export default App;
